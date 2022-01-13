8ff4552d19bf7d0cd7c5fa0c9e06449080b060a8
User <User@SERGEY>
2022-01-13 19:14:15 +0300

    Добавьте файлы проекта.

---
+++ 14 Animals 4/Animal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Animals_4
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract void Say();
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+    }
+    class Cat : Animal
+    {
+        private string say;
+        private string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Cat(string name, string say)
+            : base(name)
+        {
+            this.say = say;
+        }
+        public override void Say()
+        {
+            Console.WriteLine(say);
+        }
+    }
+    class Dog : Animal
+    {
+        private string say;
+        private string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Dog(string name, string say)
+            : base(name)
+        {
+            this.say = say;
+        }
+        public override void Say()
+        {
+            Console.WriteLine(say);
+        }
+    }
+}
---
+++ 14 Animals 4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Animals_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat Cat = new Cat("Кошка", "Мяу");
+            Console.WriteLine(Cat.ShowInfo());
+            Dog Dog = new Dog("Собака", "Гав");
+            Console.WriteLine(Dog.Say());
+            /*Cat Cat= new Cat("Кошка", "Мяу");
+            Console.WriteLine(Cat);
+            Dog Dog = new Dog("Собака", "Гав");
+            Console.WriteLine(Dog);*/
+            Console.ReadKey();
+        }
+    }
+}
